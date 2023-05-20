f1cfaea8629f6e84d4a9291ddf370ea8eda9833f
Sarmat <xonely@icloud.com>
2023-05-20 11:19:08 +0300

    update5

---
+++ src/main/java/com/sarmat/javacore/chapter05/Return.java
@@ -0,0 +1,9 @@
+package main.java.com.sarmat.javacore.chapter05;
+
+public class Return { public static void main(String[] args){
+    boolean t = true;
+    System.out.println("До возврата");
+    if(t) return;
+    System.out.println("Этот оператор не выполняется");
+}
+}
