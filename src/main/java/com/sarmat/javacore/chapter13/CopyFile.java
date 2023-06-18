e394788893948aacf5010c5a265b5f5d830a5c5c
Sarmat <xonely@icloud.com>
2023-06-18 23:07:13 +0300

    module 1.3 update2

--- src/main/java/com/sarmat/javacore/chapter13/CopyFile.java
+++ src/main/java/com/sarmat/javacore/chapter13/CopyFile.java
@@ -1,15 +1,5 @@
 package chapter13;
 
-import java.io.*;
-
-
 public class CopyFile {
-    public static void main(String[] args) {
-        int i;
-        FileInputStream fin = null;
-        FileOutputStream fout = null;
-        if (args.length != 2) {
 
-        }
-    }
 }
